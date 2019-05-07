4a4e3cfdb3601945888d0b8d1ba2bf1482cd09e2
prithvirajchowdhury <prithviraj.chowdhury16@gmail.com>
2019-05-07 07:20:36 +0530

    Python Lists

---
+++ PythonLists/Lists.py
@@ -0,0 +1,16 @@
+class Lists:
+	number_list=list()
+	def __init__(self):
+		print(self.number_list)
+
+if __name__=="__main__":
+	lists=Lists()
+	lists.number_list.append(1)
+	lists.number_list.append(2)
+	lists.number_list.append(3)
+	lists.number_list.append(4)
+
+	print(lists.number_list)
+	
+
+
